1cd0963024fab18aa37c6b9ee52bb5d7b0a3e1b2
Marcelo Garces <marcelo.rj.ni@gmail.com>
2021-04-22 00:23:57 -0300

    Add project

---
+++ src/main/java/br/com/uol/ApiFinancePagSeguroApplication.java
@@ -0,0 +1,13 @@
+package br.com.uol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinancePagSeguroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinancePagSeguroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uol/ApiFinancePagSeguroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinancePagSeguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
